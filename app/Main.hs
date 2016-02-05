a223bce51e19fdf05cf85294fe4d8ee3e6f50b5d
Martin Heuschober <epsilonhalbe@gmail.com>
2016-02-05 02:08:45 +0100

    all commands implemented, following clears messages (wtf)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-} {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
 
 module Main where
 
@@ -14,13 +14,13 @@ import           Data.Time.Clock
 import           Data.Attoparsec.Text
 import           Data.Monoid
 import qualified Data.Map as M
+import           Data.Set (Set(..))
 import qualified Data.Set as S
 import           Data.Map (Map(..), empty)
 import           Control.Monad.State
 import           Control.Lens
 import           Data.Maybe (maybe)
 import           Data.Foldable (traverse_)
-
 
 
 main :: IO ()
@@ -44,20 +44,50 @@ test = forever $
        input <- lift TIO.getLine
        now <- lift getCurrentTime
        case parseOnly command input
-           of Right c@POST{..} ->
-                 do lift $ print c
-                    let u = mempty & messages .~ M.singleton now _message
-                    modify $ M.insertWith (<>) (_message^.author) u
-              Right c@READ{..} ->
-                 do x <- gets $ M.lookup _userName
-                    lift $ maybe (TIO.putStrLn $ _userName <> " does not exist.")
-                                 (M.foldMapWithKey (\k v -> putStrLn $ displayMessage now k v))
-                                 {-(M.foldlWithKey (\_ k v-> putStrLn $ displayMessage now k v) (return ()))-}
-                                 (_messages <$> x)
-              Right c@WALL{..} ->
-                 fail "not implemented"
-              Right c -> lift $ print c
+           of Right c@POST{..}   ->
+                  do lift $ print c
+                     let u = mempty & messages .~ M.singleton now _message
+                     modify $ M.insertWith (<>) (_message^.author) u
+
+              Right c@READ{..}   ->
+                  do x <- gets $ M.lookup _userName
+                     lift $ maybe (TIO.putStrLn $ _userName <> " does not exist.")
+                                  (M.foldMapWithKey (\k v -> putStrLn $ displayMessage now k v))
+                                  {-(M.foldlWithKey (\_ k v-> putStrLn $ displayMessage now k v) (return ()))-}
+                                  (_messages <$> x)
+
+              Right c@WALL{..}   -> 
+                  do me <- gets $ M.lookup _userName
+                     x  <- get
+                     let myMsg = maybe M.empty  _messages me
+                         myFollowers  = maybe S.empty _following me :: Set UserName
+                         wall = foldr f empty $
+                                S.insert me $ S.map (`M.lookup` x) myFollowers
+                                           :: Map UTCTime Message
+                     lift $ M.foldMapWithKey (\k v -> putStrLn $ unwords
+                                                               [ T.unpack $ v^.author
+                                                               , "-"
+                                                               , displayMessage now k v]) wall
+
+              Right c@FOLLOW{..} ->
+                  do lift $ print c
+                     whom' <- gets $ M.lookup _whom
+                     case whom' of
+                         Nothing  -> lift $ TIO.putStrLn
+                                          $ T.unwords ["User:", _whom, "does not exist."]
+                         _ -> do let who' = mempty & userName .~ _who
+                                                   & following .~ S.singleton _whom 
+                                     whom' = mempty & userName .~ _whom
+                                                    & followers .~ S.singleton _who
+                                 modify $ M.insertWith (<>) _who who'
+                                 modify $ M.insertWith (<>) _whom whom'
+                                 x <- get
+                                 lift $ print x
+
               _       -> lift $ TIO.putStrLn "not yet implemented"
+
+  where f (Just x) y = M.union (_messages x) y
+        f _ y = y
 
 instance Monoid (IO ()) where
     mempty = return ()
--- src/Twotter.hs
+++ src/Twotter.hs
@@ -8,6 +8,7 @@ import           Data.Text (Text)
 import qualified Data.Text as T
 import           Control.Lens
 import           Data.Time
+import           Data.Function (on)
 import           Data.Monoid
 import           Data.Set (Set(..))
 import qualified Data.Set as S
@@ -34,6 +35,9 @@ data User = User { _userName :: UserName
 
 $(makeLenses ''User)
 
+instance Ord User where
+    compare = compare `on` _userName
+
 instance Monoid User where
     mappend u1 u2 | u1^.userName == u2^.userName = u1 & followers %~ S.union (u2^.followers)
                                                       & following %~ S.union (u2^.following)
@@ -45,7 +49,7 @@ instance Monoid User where
 type Twotter = StateT (Map UserName User) IO
 
 displayMessage :: UTCTime -> UTCTime -> Message -> String
-displayMessage now timestamp msg = show msg <> showTime (diffUTCTime now timestamp)
+displayMessage now timestamp msg = unwords [T.unpack $ msg^.content, showTime (diffUTCTime now timestamp)]
 
 showTime :: NominalDiffTime -> String
 showTime s
